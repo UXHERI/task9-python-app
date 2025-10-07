bab4b8f5c40c0f5b5c5aa769150357b2600e2121
Uzair Khan <uzairikhan2k2@gmail.com>
2025-10-07 15:55:39 +0500

    Another Change

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on AWS CodePipeline!
-            This is a new version of the app deployed directly by CodePipeline"
+    return "Hello from Flask on AWS CodePipeline! This is a change to test CodePipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

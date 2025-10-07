13edcd83af6c7a9e6f43318926768be5f7e96fac
Uzair Khan <uzairikhan2k2@gmail.com>
2025-10-07 15:04:58 +0500

    Fixed start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -8,7 +8,7 @@ echo "Killing any existing Flask processes..."
 pkill -f "python3 app.py" || echo "No Flask process running."
 
 echo "Installing dependencies..."
-pip3 install --user -r requirements.txt
+pip3 install --break-system-packages -r requirements.txt
 
 echo "Starting Flask app..."
 nohup python3 app.py > /tmp/app.log 2>&1 &
